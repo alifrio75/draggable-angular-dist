0d36568a8785309ec467c830c76a576b140162c1
webdev <geryruslandi@gmail.com>
2018-05-23 16:06:21 +0700

    init

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,85 @@
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+
+import { AppState } from '../app.service';
+import { Title } from './title';
+import { XLargeDirective } from './x-large';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  /**
+   * The selector is what angular internally uses
+   * for `document.querySelectorAll(selector)` in our index.html
+   * where, in this case, selector is the string 'home'.
+   */
+  selector: 'home',  // <home></home>
+  /**
+   * We need to tell Angular's Dependency Injection which providers are in our app.
+   */
+  providers: [
+    Title
+  ],
+  /**
+   * Our list of styles in our component. We may add more to compose many styles together.
+   */
+
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [ './home.component.css' ],
+  /**
+   * Every Angular template is first compiled by the browser before Angular runs it's compiler.
+   */
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  public closeResult: string;
+  /**
+   * Set our default values
+   */
+  public localState = { value: '' };
+  /**
+   * TypeScript public modifiers
+   */
+  constructor(
+    public appState: AppState,
+    public title: Title,
+    private modalService: NgbModal
+  ) {}
+
+  public ngOnInit() {
+    console.log('hello `Home` component');
+    /**
+     * this.title.getData().subscribe(data => this.data = data);
+     */
+  }
+
+  public open(content) {
+    this.modalService.open(content, {
+      backdrop: false,
+      centered: true,
+      windowClass: 'hidden-modal'
+    }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  public submitState(value: string) {
+    console.log('submitState', value);
+    this.appState.set('value', value);
+    this.localState.value = '';
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+}
